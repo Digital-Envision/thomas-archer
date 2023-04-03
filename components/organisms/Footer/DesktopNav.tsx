fa06f46ef1843762f958676548fd85bfe6439bf7
hakimajaman <rifqitaufiqulhakim199922@gmail.com>
2023-04-04 06:47:04 +0700

    fix: add link to navbar and footer

--- components/organisms/Footer/DesktopNav.tsx
+++ components/organisms/Footer/DesktopNav.tsx
@@ -1,7 +1,7 @@
-import { Box, Flex, SimpleGrid } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Link } from '@chakra-ui/react'
 import Text from 'components/base/Text'
 import React from 'react'
-import {NavLinksInterfaces} from '../Navbar'
+import { NavLinksInterfaces } from '../Navbar'
 
 export interface Props {
   NAV_ITEMS: Array<NavLinksInterfaces>
@@ -19,16 +19,35 @@ const DesktopNav: React.FC<Props> = ({ NAV_ITEMS }) => {
       {NAV_ITEMS.map((nav, key) => {
         return (
           <Box key={key}>
-            <Text fontWeight={700} fontSize={'12px'} lineHeight={'14px'} mb={3}>
-              {nav.label}
-            </Text>
+            <Link href={nav.href} isExternal={nav.externalLink}>
+              <Text
+                fontWeight={700}
+                fontSize={'12px'}
+                lineHeight={'14px'}
+                mb={3}
+              >
+                {nav.label}
+              </Text>
+            </Link>
             {nav.children && (
               <Flex flexDir={'column'} rowGap={3}>
                 {nav.children.map((childLink, childKey) => {
                   return (
-                    <Text key={childKey} fontSize={'12px'} lineHeight={'14px'}>
-                      {childLink.label}
-                    </Text>
+                    <Link
+                      href={childLink.href}
+                      isExternal={childLink.externalLink}
+                    >
+                      <Text
+                        key={childKey}
+                        fontSize={'12px'}
+                        lineHeight={'14px'}
+                        _hover={{
+                          textDecoration: 'underline',
+                        }}
+                      >
+                        {childLink.label}
+                      </Text>
+                    </Link>
                   )
                 })}
               </Flex>
