9cf6570ac62110edab988afa6566f231df21beb6
hakimajaman <rifqitaufiqulhakim199922@gmail.com>
2023-06-28 21:59:35 +0700

    feat: add infinite transition for floorplan slider

--- components/organisms/FloorPlanListing/Slider.tsx
+++ components/organisms/FloorPlanListing/Slider.tsx
@@ -4,17 +4,97 @@ import ButtonIcon, {
   Variants as ButtonIconVariants,
 } from 'components/base/ButtonIcon'
 import { HiOutlineArrowLeft, HiOutlineArrowRight } from 'react-icons/hi2'
-import React from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import Text from 'components/base/Text'
 
-const Slider = ({
-  images = [],
-  title,
-  href = '#',
-  handlePrev,
-  handleNext,
-  slide,
-}) => {
+const Slider = ({ listImages = [], title, href = '#' }) => {
+  const imageSlider = useRef<HTMLDivElement>()
+
+  const images = useMemo(() => {
+    return [listImages[listImages.length - 1], ...listImages, listImages[0]]
+  }, [listImages])
+
+  const [slide, setSlide] = useState(1)
+  const [hasTransition, setHasTransition] = useState(false)
+
+  const [preventRightClick, setPreventRightClick] = useState(false)
+  const [preventLeftClick, setPreventLeftClick] = useState(false)
+
+  const handleScrollLeft = () => {
+    const length = images.length
+
+    if (slide !== 0) {
+      setPreventLeftClick(true)
+
+      setHasTransition(true)
+      setSlide((prevIndex) => {
+        return prevIndex === 0 ? length - 1 : prevIndex - 1
+      })
+
+      setTimeout(() => {
+        setPreventLeftClick(false)
+      }, 300)
+    }
+  }
+
+  const handleScrollRight = () => {
+    const length = images.length
+
+    if (slide !== length - 1) {
+      setPreventRightClick(true)
+
+      setHasTransition(true)
+      setSlide((prevIndex) => {
+        return prevIndex === length - 1 ? 0 : prevIndex + 1
+      })
+
+      setTimeout(() => {
+        setPreventRightClick(false)
+      }, 300)
+    }
+  }
+
+  const handlePrev = () => {
+    if (!preventLeftClick) {
+      handleScrollLeft()
+    }
+  }
+
+  const handleNext = () => {
+    if (!preventRightClick) {
+      handleScrollRight()
+    }
+  }
+
+  const handleTransition = () => {
+    const length = images?.length
+
+    if (imageSlider?.current) {
+      if (slide === length - 1) {
+        setHasTransition(false)
+        setSlide(1)
+      } else if (slide === 0) {
+        setHasTransition(false)
+        setSlide(2)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (imageSlider?.current) {
+      imageSlider?.current?.addEventListener('transitionend', handleTransition)
+    }
+
+    return () => {
+      if (imageSlider?.current) {
+        imageSlider?.current?.removeEventListener(
+          'transitionend',
+          handleTransition
+        )
+      }
+    }
+  }, [slide])
+
   return (
     images?.length > 0 && (
       <Box mt={5}>
@@ -45,7 +125,8 @@ const Slider = ({
                     width="100%"
                     height="100%"
                     transform={`translateX(${(index - slide) * 100}%)`}
-                    transition="transform 0.5s"
+                    transition={`${hasTransition ? 'transform 0.5s' : 'none'}`}
+                    ref={imageSlider}
                   >
                     <Flex
                       flexDir={'column'}
@@ -72,7 +153,10 @@ const Slider = ({
                 bg={'transparent'}
                 onClick={handlePrev}
                 visibility={images.length < 2 ? 'hidden' : 'visible'}
-                fontSize={{ lg: '30px' }}
+                fontSize={{ base: '20px', lg: '15px' }}
+                size={'sm'}
+                height={{ base: '40px', lg: '32px' }}
+                width={{ base: '40px', lg: '30px' }}
               >
                 <HiOutlineArrowLeft />
               </ButtonIcon>
@@ -83,7 +167,10 @@ const Slider = ({
                 onClick={handleNext}
                 visibility={images.length < 2 ? 'hidden' : 'visible'}
                 marginLeft={'auto'}
-                fontSize={{ lg: '30px' }}
+                fontSize={{ base: '20px', lg: '15px' }}
+                size={'sm'}
+                height={{ base: '40px', lg: '32px' }}
+                width={{ base: '40px', lg: '30px' }}
               >
                 <HiOutlineArrowRight />
               </ButtonIcon>
